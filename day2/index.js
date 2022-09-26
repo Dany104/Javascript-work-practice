625ff6e7e5cf2a2f28deacc5b9453b9d090af89d
Syed Danish Ali <79353146+Dany104@users.noreply.github.com>
2022-09-26 11:35:24 +0500

    javascript work till Increment operator

---
+++ day2/index.js
@@ -0,0 +1,104 @@
+/*** Section 3 Values and variables in javascript ***/
+
+/** var myName = 'Syed Danish Ali' ;
+console.log(myName);
+
+// Challenge Quiz
+
+var _myname= 'Danish';
+var _1my__Name = 'Syed';
+// var 1myName =  'Ali';
+var $myName = 'danyali'
+console.log(_myname); **/
+
+/***  Section 4 Data types in JS  ***/
+
+/* var myName = "Danish Ali";
+ console.log(myName);
+
+ //   typeof operator = to find data type
+
+console.log(typeof(myName));
+*/
+/*
+var myAge= 29;
+console.log(myAge);
+console.log(typeof(myAge));*/
+
+/* var iAmDanish = true;
+console.log(iAmDanish);
+console.log(typeof(iAmDanish));*/
+
+/* var iAmDanishs = false
+console.log(iAmDanishs);
+console.log(typeof(iAmDanishs)); */
+
+/***       Challenge number 2 ***/
+/*
+var a = 10+"20"
+var b = 9 - "5"
+var c = "Java" + "Script"
+var d = " " + " "
+var e = " " + 0
+var f = "Danish" - "Ali"
+var g = true + true
+var h = true + false
+var i = false + true
+var j= false - true
+console.log(j);
+ */
+/* Interview Question: What is the difference between null and undefined */
+/*var iamUseLess = null;
+console.log(iamUseLess);
+console.log(typeof(iamUseLess));
+var iAmStandby;
+console.log(iAmStandby);
+console.log(typeof(iAmStandby));*/
+
+/* Q2) What is Nan?
+ANS) Nan is a property of Global object.
+In other words, It is a variable in global scope.
+The initial value of Nan is ' Not A Number'
+*/
+
+/*var myNumber= '0093710312247';
+var myName= 'danish';
+console.log(isNaN(myNumber));
+console.log(isNaN(myName));
+
+if (isNaN(myName)){
+    console.log("plz enter valid number");
+}*/
+
+// NAN practice
+/*
+console.log(NaN === NaN);
+console.log(Number.NaN=== NaN);
+console.log(isNaN(NaN));
+console.log(isNaN(Number.NaN));
+console.log(Number.isNaN(NaN));
+*/
+
+// Asignment operator
+// An assignment operator assigns a value to its
+// left operand based on the value of its right operand.
+// The simple assignment operator is equal(=).
+
+/*var x = 5;
+var y = 5;
+console.log("Is both x and y are equal : ", x==y);
+*/
+// Arithmetic operator
+// Arithmetic operator takes numerical values
+// (either literals or variables) as their operands and
+// returns a single numerical value.
+
+/*console.log(5+5);
+console.log(20-15);
+console.log(20/5);
+console.log(5*6);
+console.log("Modulus operator returns devision remainder : " , 51%5);
+*/
+
+// Increment Operator
+// Operator : x++ or ++x or x-- or --x
